1885b0c4d10d73d8a2670ea9150909601ca1fa12
Tom Eccles <t@freedommail.info>
2014-06-29 21:00:48 +0100

    Finished. I found that the append flag takes presedence over an lseek followed by a write.
    This is because the append is atomic

--- append.c
+++ append.c
@@ -1,6 +1,10 @@
 #include <stdio.h>		// for printf and fflush
 #include <errno.h>		// for errno
 #include <stdlib.h>		// for exit
+#include <sys/types.h>	// for open and lseek
+#include <sys/stat.h>	// for open
+#include <fcntl.h>		// for open
+#include <unistd.h>		// for lseek
 
 void errExit(char* errMsg)
 {
@@ -18,7 +22,18 @@ void errExit(char* errMsg)
 
 int main(int argc, char* argv[])
 {
-	
+	// open file for writing
+	int fd = open("testFile", O_APPEND | O_WRONLY);
+	if (fd == -1)
+		errExit("open");
+
+	// attempt to seek to the beginning
+	if (lseek(fd, 0, SEEK_SET) == -1)
+		errExit("lseek");
+
+	// write some data
+	if (write(fd, "new\n", 5) == -1)
+		errExit("write");
 
 	exit(EXIT_SUCCESS);
 }
